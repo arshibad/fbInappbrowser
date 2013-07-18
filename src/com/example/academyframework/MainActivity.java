4d6f7c270f8c418807640de4599229281c0adef7
arshibad <arshibad1991@gmail.com>
2013-07-18 11:52:44 +0530

    1st step connect to facebook login and get the public details

---
+++ src/com/example/academyframework/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.academyframework;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import org.apache.cordova.*;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    
+}
